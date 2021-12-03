e799c88aeb76203a2ff8f2886cd1ce198ad80d4f
IT-Prince <>
2021-12-04 01:53:37 +0300

    Calculate the LP amount from

--- src/components/LockVaultModal/LockVaultModal.tsx
+++ src/components/LockVaultModal/LockVaultModal.tsx
@@ -47,7 +47,7 @@ const LockVaultModal = ({ data }: LockVaultModalProps) => {
   const [usdrAmount, setUSDrAmount] = React.useState(0);
   const [maxUSDrAmount, setMaxUSDrAmount] = React.useState(0);
   const tokenPrice = usePrice(data.mint);
-
+  const [initCollAmount, setInitCollAmount] = React.useState(0);
   const collMint = useMint(data.mint);
   const usdrMint = useMint(USDR_MINT_KEY);
 
@@ -57,9 +57,18 @@ const LockVaultModal = ({ data }: LockVaultModalProps) => {
   const [disableDeposit, setDisableDeposit] = useState(false);
 
   useEffect(() => {
-    const maxAmount = Math.ceil(getUSDrAmount(data.risk, tokenPrice * lpAmount) * 1000) / 1000;
+    const maxAmount = Math.ceil(getUSDrAmount(data.riskPercentage, tokenPrice * lpAmount) * 1000) / 1000;
     setMaxUSDrAmount(maxAmount);
   }, [tokenPrice, lpAmount]);
+
+  useEffect(() => {
+    if (tokenPrice) {
+      const initLPAmount = Math.ceil((10 / tokenPrice) * 1000) / 1000;
+      console.log('Risk', data.riskPercentage);
+      setMaxUSDrAmount(Math.ceil(getUSDrAmount(data.riskPercentage, tokenPrice * initLPAmount) * 1000) / 1000);
+      setInitCollAmount(initLPAmount);
+    }
+  }, [tokenPrice]);
 
   useEffect(() => {
     if (wallet && wallet.publicKey) {
@@ -74,10 +83,11 @@ const LockVaultModal = ({ data }: LockVaultModalProps) => {
       });
       if (collAccount) {
         const tokenAmount = new TokenAmount(collAccount.info.amount + '', collMint?.decimals);
+        console.log(tokenAmount);
         setCollBalance(Math.ceil(parseFloat(tokenAmount.fixed()) * 100) / 100);
       }
     }
-  });
+  }, [wallet, collAccount, connection, collMint]);
 
   useEffect(() => {
     getTokenVaultByMint(connection, data.mint).then((res) => {
@@ -138,6 +148,7 @@ const LockVaultModal = ({ data }: LockVaultModalProps) => {
             <label className="lockvaultmodal__label1 mb-2">How much would you like to lock up?</label>
             <CustomInput
               appendStr="Max"
+              initValue={'' + initCollAmount}
               appendValueStr={'' + collBalance}
               tokenStr={`${data.title} LP`}
               onTextChange={(value) => setLPAmount(Number(value))}
