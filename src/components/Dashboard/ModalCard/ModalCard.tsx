7c5d4bf0ed4fae44098fbdf644d227f0c3fe9240
WonderDev21 <wonder.dev21@gmail.com>
2021-12-15 02:54:01 +0900

    updated faucet

--- src/components/Dashboard/ModalCard/ModalCard.tsx
+++ src/components/Dashboard/ModalCard/ModalCard.tsx
@@ -89,7 +89,7 @@ const ModalCard = ({
             <div className="modalCard__header_tokenName">
               {/* <p>{data.tokenNames}</p> */}
               {/* <h6>{data.tokenValue}</h6> */}
-              {type === 'borrow_payback' && <p>${paybackData.usdrValue}</p>}
+              {type === 'borrow_payback' && <p>${Math.ceil(paybackData.usdrValue * 100) / 100} USDr</p>}
             </div>
           </div>
           <div>
