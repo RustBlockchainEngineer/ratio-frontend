efe63cdf299f27e3f29c7433c817a08fae3e818e
Mariano Aguero <mariano.aguero@gmail.com>
2022-01-14 00:04:31 -0300

    Refactor name

--- src/hooks/useMercurialRewards.ts
+++ src/hooks/useMercurialRewards.ts
@@ -1,13 +1,13 @@
 import { useQuery } from '@apollo/react-hooks';
 import { queryGetRewardsForWalletWithPaging } from '../queries/rewards';
 
-export enum RewardStatus {
+export enum MercurialRewardStatus {
   claimed = 'claimed',
   unclaimed = 'unclaimed',
   pending = 'pending',
 }
 
-export const useRewards = (address: string | null | undefined, status = RewardStatus.unclaimed) => {
+export const useMercurialRewards = (address: string | null | undefined, status = MercurialRewardStatus.unclaimed) => {
   const { data, error, loading } = useQuery(queryGetRewardsForWalletWithPaging, {
     variables: { address, status },
     fetchPolicy: 'no-cache',
