358fe8a90d92c8d3cd5f8cb43ab4fbb06f854830
Lucas Marc <lucas@thinkanddev.com>
2022-01-20 18:36:43 -0300

    Added authentication and authorization on the frontend

--- src/App.tsx
+++ src/App.tsx
@@ -11,47 +11,49 @@ import Faucet from './pages/Faucet';
 import AdminPanel from './pages/AdminPanel';
 import { AuthProvider } from './contexts/auth';
 import { RaydiumPoolProvider, SaberPoolProvider, MercurialPoolProvider, OrcaPoolProvider } from './contexts/pools';
+import { AuthContextProvider as APIAuthContextProvider } from './contexts/authAPI';
 import { PriceProvider } from './contexts/price';
 import { MercurialAPIProvider } from './contexts/mercurialAPI';
 import ProtectedRoute from './components/ProtectedRoute';
 
 dotenv.config();
-
 const App: React.FC = () => {
   return (
     <ConnectionProvider>
       <AuthProvider>
         <WalletProvider>
-          <AccountsProvider>
-            <RaydiumPoolProvider>
-              <SaberPoolProvider>
-                <MercurialPoolProvider>
-                  <OrcaPoolProvider>
-                    <PriceProvider>
-                      <MarketProvider>
-                        <MercurialAPIProvider>
-                          <ThemeProvider>
-                            <Router>
-                              <div>
-                                <Switch>
-                                  <Route path="/dashboard" component={Layer} />
-                                  <Route path="/faucet" exact component={Faucet} />
-                                  <Route path="/adminpanel" exact component={AdminPanel} />
-                                  <Route exact path="/">
-                                    <Redirect to="/dashboard" />
-                                  </Route>
-                                </Switch>
-                              </div>
-                            </Router>
-                          </ThemeProvider>
-                        </MercurialAPIProvider>
-                      </MarketProvider>
-                    </PriceProvider>
-                  </OrcaPoolProvider>
-                </MercurialPoolProvider>
-              </SaberPoolProvider>
-            </RaydiumPoolProvider>
-          </AccountsProvider>
+          <APIAuthContextProvider>
+            <AccountsProvider>
+              <RaydiumPoolProvider>
+                <SaberPoolProvider>
+                  <MercurialPoolProvider>
+                    <OrcaPoolProvider>
+                      <PriceProvider>
+                        <MarketProvider>
+                          <MercurialAPIProvider>
+                            <ThemeProvider>
+                              <Router>
+                                <div>
+                                  <Switch>
+                                    <Route path="/dashboard" component={Layer} />
+                                    <Route path="/faucet" exact component={Faucet} />
+                                    <ProtectedRoute role="ADMIN" exact path="/adminpanel" component={AdminPanel} />
+                                    <Route exact path="/">
+                                      <Redirect to="/dashboard" />
+                                    </Route>
+                                  </Switch>
+                                </div>
+                              </Router>
+                            </ThemeProvider>
+                          </MercurialAPIProvider>
+                        </MarketProvider>
+                      </PriceProvider>
+                    </OrcaPoolProvider>
+                  </MercurialPoolProvider>
+                </SaberPoolProvider>
+              </RaydiumPoolProvider>
+            </AccountsProvider>
+          </APIAuthContextProvider>
         </WalletProvider>
       </AuthProvider>
     </ConnectionProvider>
