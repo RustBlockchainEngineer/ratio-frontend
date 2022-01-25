b1d7a8d49fc0408bbf255f43601d9fd9cbd05956
Lucas Marc <lucas@thinkanddev.com>
2022-01-25 10:55:42 -0300

    Fixed dark mode error, and added conditional sidebar based on connected status

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -4,12 +4,14 @@ import { useDispatch } from 'react-redux';
 import { actionTypes } from '../../features/wallet';
 import { ThemeContext } from '../../contexts/ThemeContext';
 import Navbar from '../../components/Navbar';
+import { useWallet } from '../../contexts/wallet';
 
 const ErrorPage = ({ headline, message, description }: any) => {
   const theme = React.useContext(ThemeContext);
   const { darkMode } = theme.state;
   const [menuOpen, setMenuOpen] = React.useState(false);
   const [collapseFlag, setCollapseFlag] = React.useState(false);
+  const { connected } = useWallet();
 
   const dispatch = useDispatch();
 
@@ -29,14 +31,16 @@ const ErrorPage = ({ headline, message, description }: any) => {
     <div className="error_page" data-theme={darkMode ? 'dark' : 'light'}>
       <div className="error_page_container">
         <Header onClickWalletBtn={onClickWalletBtn} darkMode={darkMode} />
-        <Navbar
-          darkMode={darkMode}
-          onClickWalletBtn={onClickWalletBtn}
-          clickMenuItem={clickMenuTrigger}
-          open={menuOpen}
-          collapseFlag={collapseFlag}
-          setCollapseFlag={onCollapseMenu}
-        />
+        {connected && (
+          <Navbar
+            darkMode={darkMode}
+            onClickWalletBtn={onClickWalletBtn}
+            clickMenuItem={clickMenuTrigger}
+            open={menuOpen}
+            collapseFlag={collapseFlag}
+            setCollapseFlag={onCollapseMenu}
+          />
+        )}
         <div className="error_page_content">
           <div className="error_page_headline">{headline}</div>
           <div className="error_page_message">{message}</div>
