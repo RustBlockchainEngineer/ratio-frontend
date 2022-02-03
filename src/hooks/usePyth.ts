13d665027b12127008d1023dae06cee3065c1292
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-02-03 20:13:09 -0300

    feat(usePyth): add callback

--- src/hooks/usePyth.ts
+++ src/hooks/usePyth.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { getPythProgramKeyForCluster, PythConnection } from "@pythnetwork/client";
 import { Connection, Cluster } from '@solana/web3.js';
 import { useConnectionConfig } from '../contexts/connection';
@@ -8,19 +8,25 @@ import { useConnectionConfig } from '../contexts/connection';
 export function usePyth(connection: Connection){
     const [usdPrice,setPrice] = useState<any>({price: 'not defined', confidence:'not defined'});
     const pythConnection = new PythConnection(connection,getPythProgramKeyForCluster('devnet'));
-    
-    pythConnection.onPriceChange((product,price) => {
-        /**
-         * Look for specific changes in USDC/USD
-         * Make sure the price has varied
-         * */
-        if(product?.description === 'USDC/USD' && product?.symbol === 'Crypto.USDC/USD'){
+
+    const handleOnPriceChange = useCallback((product,price) => {
+         /**
+             * Look for specific changes in USDC/USD
+             * Make sure the price has varied
+             * */
+          if(product?.description === 'USDC/USD' && product?.symbol === 'Crypto.USDC/USD'){
             setPrice({
                 price: price?.price,
                 confidence: price?.confidence
             })
         }
-    })
+    },[]);
+    
+    useEffect(() => {
+        pythConnection.onPriceChange((product,price) => {
+            handleOnPriceChange(product,price);
+        })
+    },[handleOnPriceChange]);
 
     // Start listening for price change events.
     pythConnection.start();
