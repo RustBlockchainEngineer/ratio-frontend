b83b25e226b09f3518f119fd3d3d041353846b4b
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-02-03 18:16:19 -0300

    wip, remain to merge other pr and test

--- src/hooks/usePyth.ts
+++ src/hooks/usePyth.ts
@@ -1,6 +1,29 @@
 /* eslint-disable prettier/prettier */
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { getPythProgramKeyForCluster, PythConnection } from "@pythnetwork/client";
+import { Connection, Cluster } from '@solana/web3.js';
+import { useConnectionConfig } from '../contexts/connection';
 
-export function usePyth(){
+
+export function usePyth(connection: Connection){
+    const [usdPrice,setPrice] = useState<any>({price: 'not defined', confidence:'not defined'});
+    const pythConnection = new PythConnection(connection,getPythProgramKeyForCluster('devnet'));
     
+    pythConnection.onPriceChange((product,price) => {
+        /**
+         * Look for specific changes in USDC/USD
+         * Make sure the price has varied
+         * */
+        if(product?.description === 'USDC/USD' && product?.symbol === 'Crypto.USDC/USD'){
+            setPrice({
+                price: price?.price,
+                confidence: price?.confidence
+            })
+        }
+    })
+
+    // Start listening for price change events.
+    pythConnection.start();
+
+    return usdPrice;
 }
