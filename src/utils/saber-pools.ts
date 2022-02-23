c1b13cc58d8f3a1d127369e24510e6e41fb6006a
IT-Prince <hitman0290@gmail.com>
2022-02-23 15:52:52 +0100

    Fixed too many req

--- src/utils/saber-pools.ts
+++ src/utils/saber-pools.ts
@@ -2,6 +2,7 @@
 import { AccountInfo, Connection, PublicKey } from '@solana/web3.js';
 import { StableSwap, loadExchangeInfoFromSwapAccount } from '@saberhq/stableswap-sdk';
 import { getDevnetPools, getMainnetPools } from './saber/ids';
+import { sleep } from './utils';
 // import { SABER_QUARRY_NEW } from './saber/constants';
 
 export async function loadSaberSwap(conn: Connection, swapAccount: PublicKey) {
@@ -42,32 +43,20 @@ export async function getSaberSwapPoolsInfo(conn: Connection, connEnv: string) {
     const swapAccount = new PublicKey(pools[i].swapAddress);
     const poolAddr = pools[i].quarryAddress;
 
-    // if (poolAddr === SABER_QUARRY_NEW) {
-    //   swapPoolsInfo[`${pools[i].name}`] = {
-    //     poolAddr,
-    //     tokenAName: 'Token A',
-    //     tokenAAddress: 'EJwZgeZrdC8TXTQbQBoL6bfuAnFUUy1PVCMB4DYPzVaS',
-    //     tokenAAmount: '1000',
-    //     tokenBName: 'Token B',
-    //     tokenBAddress: 'EJwZgeZrdC8TXTQbQBoL6bfuAnFUUy1PVCMB4DYPzVaS',
-    //     tokenBAmount: '1000',
-    //     tvl: '20',
-    //   };
-    // } else {
-      const { tokenAName, tokenAAddress, tokenAAmount, tokenBName, tokenBAddress, tokenBAmount, tvl } =
-        await getSaberSwapPoolInfo(conn, swapAccount);
+    const { tokenAName, tokenAAddress, tokenAAmount, tokenBName, tokenBAddress, tokenBAmount, tvl } =
+      await getSaberSwapPoolInfo(conn, swapAccount);
 
-      swapPoolsInfo[`${pools[i].name}`] = {
-        poolAddr,
-        tokenAName,
-        tokenAAddress,
-        tokenAAmount,
-        tokenBName,
-        tokenBAddress,
-        tokenBAmount,
-        tvl,
-      };
-    // }
+    swapPoolsInfo[`${pools[i].name}`] = {
+      poolAddr,
+      tokenAName,
+      tokenAAddress,
+      tokenAAmount,
+      tokenBName,
+      tokenBAddress,
+      tokenBAmount,
+      tvl,
+    };
+    await sleep(600);
   }
   return swapPoolsInfo;
 }
