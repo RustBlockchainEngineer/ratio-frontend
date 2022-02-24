f8d8ac0e83a48130dfff520352e67def82f755e7
Alex Hoffnung <alexhoffnung@users.noreply.github.com>
2022-02-24 13:39:15 -0500

    Update Devnet Guide

--- src/components/GuideModal/GuideModal.tsx
+++ src/components/GuideModal/GuideModal.tsx
@@ -12,7 +12,7 @@ const GuideModal = () => {
 
   return (
     <div className="guidemodal">
-      <Button disabled={!connected} className="button--blue guidemodal__button" onClick={() => setShow(!show)}>
+      <Button disabled={!connected} className="button--fill guidemodal__button" onClick={() => setShow(!show)}>
         Guide
       </Button>
 
@@ -29,39 +29,30 @@ const GuideModal = () => {
             <IoMdClose size={32} className="guidemodal__header-close" onClick={() => setShow(false)} />
             <h2 className="guidemodal__title">Guide</h2>
             <h6>
-              We are thrilled to release the Devnet version of the Ratio platform. We hope this preview into our project
-              is as exciting to you as it is to us. The Devnet project provides a chance to view our user interface and
-              get a sense of the user experience we provide.
+              We are thrilled to release the Devnet version 2.0 of the Ratio platform. We hope this preview into our project is as exciting to you as it is to us. The Devnet project provides a chance to view our user interface and get a sense of the user experience we provide.
             </h6>
 
             <h6>
-              Try out our functionality by providing liquidity to our LP vaults and minting USDr in exchange. You have
-              access to vaults for 4 LP tokens: USDC-USDR, ETH-SOL, ATLAS-RAY, and SAMO-RAY. For Devnet testing, users
-              can lock $10 worth of any of the tokens (0.1 tokens) in a single deposit and then mint a maximum amount of
-              USDr corresponding to the risk factor.
+              Try out our functionality by providing Saber liquidity to our LP vaults and minting USDr in exchange. You have access to vaults for 4 LP tokens: USDC-USDT, USDC-PAI, USDC-CASH, and USDT-CASH. The LP tokens are each valued at $1 for this release. The faucet will mint $10 worth of tokens in a single transaction. When you deposit these tokens into an available vault, this vault is then considered active and you may mint an amount of USDr corresponding to the collateralization ratio.
             </h6>
 
             <h6>
-              A sophisticated risk analysis algorithm is a premier feature offered to users on the Ratio platform. As
-              such, in our Devnet version we assign a static risk value to each of our 4 LP tokens prior to releasing
-              our integrated risk analysis functionality on Mainnet.
+              A sophisticated risk analysis algorithm is a premier feature offered to users on the Ratio platform. As such, in our Devnet version we assign a Ratio Risk Rating to each of the Saber LP tokens.
             </h6>
 
             <h6>
-              In order for a user to withdraw their collateral, their full debt must be paid back, which can be done as
-              a single transaction. The user can then withdraw the total amount of collateral locked in their vault,
-              also in a single transaction.
+              On depositing LP into a Ratio vault, the tokens will be immediately sent to the corresponding Saber yield farm and will begin earning rewards, which can be harvested at any time.
             </h6>
+
             <div className="guidemodal__body">
               <p>Step 1: Switch your wallet from a mainnet wallet to devnet </p>
               <p>Step 2: Go to solfaucet.com to obtain devnet sol </p>
               <p>Step 3: Connect your wallet to dev.ratio.finance</p>
               <p>Step 4: Go to the faucet on the site </p>
               <p>Step 5: Mint any of the LP devnet tokens </p>
-              <p>Step 6: Use the app to deposit, mint, pay back, and withdraw LP </p>
+              <p>Step 6: Use the app to deposit, mint, pay back, withdraw, and harvest. </p>
               <h6 className="mt-3">
-                Note: Note: Phantom wallets will give the best user experience as token names and icons will be imported
-                to wallet.
+                Note: Note: Phantom wallets will give the best user experience as token names and icons will be imported to wallet.
               </h6>
             </div>
             <div className="guidemodal__btnBox row">
@@ -79,3 +70,4 @@ const GuideModal = () => {
 };
 
 export default GuideModal;
+
