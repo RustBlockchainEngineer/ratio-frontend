90407be166821dc39f84f4f87996d0c670969c46
IT-Prince <hitman0290@gmail.com>
2022-02-25 14:31:17 +0100

    Added all pools to context

--- src/contexts/pools.tsx
+++ src/contexts/pools.tsx
@@ -1,31 +1,81 @@
 import { Connection } from '@solana/web3.js';
 import React, { useContext, useEffect, useState } from 'react';
+import { getMercurialSwapPoolsInfo } from '../utils/mercurial-pools';
+import { getOrcaSwapPoolInfo } from '../utils/orca-pools';
 import { getRaydiumPools, getRaydiumPoolsInfo } from '../utils/ray-pools';
-import { ENDPOINTS } from './connection';
+import { getSaberSwapPoolsInfo } from '../utils/saber-pools';
+import { useConnection, useConnectionConfig } from './connection';
 
 interface PoolsConfig {
-  pools: any;
+  raydiumPools: any;
+  saberPools: any;
+  orcaPools: any;
+  mercurialPools: any;
 }
 
 const PoolsContext = React.createContext<PoolsConfig>({
-  pools: {},
+  raydiumPools: {},
+  saberPools: [],
+  orcaPools: [],
+  mercurialPools: [],
 });
 
-export function RaydiumPoolProvider({ children = undefined as any }) {
-  const [pools, setPools] = useState<any>(null);
-  const [conn, setConnection] = useState<Connection>(new Connection(ENDPOINTS[0].endpoint));
+export function PoolProvider({ children = undefined as any }) {
+  const connection = useConnection();
+  const connectionConfig = useConnectionConfig();
+
+  const [raydiumPools, setRaydiumPools] = useState<any>(null);
+  const [saberPools, setSaberPools] = useState<any>(null);
+  const [orcaPools, setOrcaPools] = useState<any>(null);
+  const [mercurialPools, setMercurialPools] = useState<any>(null);
+
   useEffect(() => {
     try {
-      getRaydiumPools(conn).then((res: any) => {
-        setPools(res);
+      getRaydiumPools(connection).then((res: any) => {
+        console.log('Loaded Raydium pools ***', res);
+        setRaydiumPools(res);
       });
     } catch (e) {}
-  }, [conn]);
+  }, [connection]);
+
+  useEffect(() => {
+    try {
+      getSaberSwapPoolsInfo(connection, connectionConfig.env).then((res: any) => {
+        console.log('Loaded Saber pools ***', res);
+        setSaberPools(res);
+      });
+    } catch (e) {}
+  }, [connection]);
+
+  useEffect(() => {
+    try {
+      getOrcaSwapPoolInfo().then((res: any) => {
+        console.log('Loaded Orca pools ***', res);
+        setOrcaPools(res);
+      });
+    } catch (e) {
+      console.error('ORCA ERROR while obtaining pools info', e);
+    }
+  }, [connection]);
+
+  useEffect(() => {
+    try {
+      getMercurialSwapPoolsInfo().then((res: any) => {
+        console.log('Loaded Mercurial pools ***', res);
+        setMercurialPools(res);
+      });
+    } catch (e) {
+      console.error('Mercurial ERROR while obtaining pools info', e);
+    }
+  }, [connection]);
 
   return (
     <PoolsContext.Provider
       value={{
-        pools,
+        raydiumPools,
+        saberPools,
+        orcaPools,
+        mercurialPools,
       }}
     >
       {children}
@@ -34,5 +84,17 @@ export function RaydiumPoolProvider({ children = undefined as any }) {
 }
 
 export function useRaydiumPools() {
-  return useContext(PoolsContext)?.pools;
+  return useContext(PoolsContext)?.raydiumPools;
+}
+
+export function useSaberPools() {
+  return useContext(PoolsContext)?.saberPools;
+}
+
+export function useOrcaPools() {
+  return useContext(PoolsContext)?.orcaPools;
+}
+
+export function useMercurialPools() {
+  return useContext(PoolsContext)?.mercurialPools;
 }
