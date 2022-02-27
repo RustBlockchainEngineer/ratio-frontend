ce42d9c45e18388916f6fdf2bdea5d230021565e
IT-Prince <hitman0290@gmail.com>
2022-02-27 15:28:59 +0100

    Update harvest fees

--- src/pages/VaultDashboard/VaultDashboard.tsx
+++ src/pages/VaultDashboard/VaultDashboard.tsx
@@ -121,8 +121,6 @@ const VaultDashboard = () => {
   }, [wallet, connected, updateStateFlag]);
 
   useEffect(() => {
-    console.log('USDC_CASH', collMint);
-
     if (wallet && wallet.publicKey && collMint && collAccount) {
       const tokenAmount = new TokenAmount(collAccount.info.amount + '', collMint?.decimals);
       setLpWalletBalance(Number(tokenAmount.fixed()));
