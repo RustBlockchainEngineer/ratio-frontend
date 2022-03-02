8e658526fd17b9ba8741696e6189311f6f3f8d8f
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-03-02 18:17:10 -0300

    wip hook

---
+++ src/hooks/useFetchSaberLpPrices.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { API_ENDPOINT } from '../constants';
+import { useAuthContextProvider } from '../contexts/authAPI';
+
+function makeRatioApiEndpointSaberLpPrices(): string {
+  return `${API_ENDPOINT}/saberlpprices`;
+}
+
+export const useFetchSaberLpPrices = () => {
+  const [lpPrices, setLpPrices] = useState<any>({ DATA: 'DATA NOT LOADED YET' });
+  const [error, setError] = useState<any>(null);
+  const { accessToken } = useAuthContextProvider();
+
+  useEffect(() => {
+    let cancelRequest = false;
+    async function getLpPrices() {
+      try {
+        const res = await fetch(makeRatioApiEndpointSaberLpPrices(), {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': JSON.stringify(accessToken),
+          },
+          method: 'GET',
+        });
+        const tokenPrices = await res.json();
+        if (cancelRequest) return;
+        setLpPrices(tokenPrices);
+        setError(null);
+      } catch (error) {
+        if (cancelRequest) return;
+        setError(error);
+      }
+    }
+
+    getLpPrices();
+    return function cleanup() {
+      cancelRequest = true;
+    };
+  }, []);
+
+  return {
+    lpPrices,
+    error,
+  };
+};
