a4a23cfd7ed8b332bb9ca140231637a09b37b326
Mariano Aguero <mariano.aguero@gmail.com>
2022-03-24 12:45:45 -0300

    fix: add improvement with try catch and async/await

--- src/components/Dashboard/AmountPanel/TokensEarned.tsx
+++ src/components/Dashboard/AmountPanel/TokensEarned.tsx
@@ -26,20 +26,21 @@ const TokensEarned = ({ data }: any) => {
   const userState = useUserInfo(data.mintAddress);
   const { saberPrice, status: saberPriceStatus, error: saberPriceError } = useFetchSaberPrice();
 
-  const harvest = () => {
-    console.log('harvesting');
-    PoolManagerFactory?.harvestReward(connection, wallet, vault as LPair)
-      .then(() => {
-        updateRFStates(UPDATE_REWARD_STATE, data.mintAddress);
-      })
-      .catch((e) => {
-        console.log(e);
-        if (isWalletApproveError(e)) toast.warn('Wallet is not approved!');
-        else toast.error('Transaction Error!');
-      })
-      .finally(() => {
-        toast.success('Successfully Harvested!');
-      });
+  const harvest = async () => {
+    try {
+      if (!PoolManagerFactory || !PoolManagerFactory?.harvestReward) {
+        throw new Error('Pool manager factory not initialized');
+      }
+
+      console.log('Harvesting...');
+      await PoolManagerFactory?.harvestReward(connection, wallet, vault as LPair);
+      await updateRFStates(UPDATE_REWARD_STATE, data.mintAddress);
+      toast.success('Successfully Harvested!');
+    } catch (err) {
+      console.error(err);
+      if (isWalletApproveError(err)) toast.warn('Wallet is not approved!');
+      else toast.error('Transaction Error!');
+    }
   };
 
   const getTokenNameByPlatform = (name: string) => {
