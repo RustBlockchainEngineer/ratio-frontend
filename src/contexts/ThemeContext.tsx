bbfa37ef3aedbd1af524f7028e0d549b3d8ead4e
WonderDev21 <wonder.dev21@gmail.com>
2022-04-08 19:41:03 +0900

    fix: light/darkmode with localstorage

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,6 +1,8 @@
-import React, { createContext, useReducer } from 'react';
+import React, { useCallback, createContext, useReducer } from 'react';
+import { useLocalStorage } from 'react-use';
 
 export const ThemeContext = createContext<any>(null);
+const LOCAL_STORAGE_KEY = 'theme';
 
 const initialState = {
   darkMode: false,
@@ -17,8 +19,24 @@ const themeReducer = (state: any, action: any) => {
   }
 };
 
+const usePersistReducer = () => {
+  const [savedState, saveState] = useLocalStorage(LOCAL_STORAGE_KEY, initialState);
+  const reducerLocalStorage = useCallback(
+    (state, action) => {
+      const newState = themeReducer(state, action);
+
+      saveState(newState);
+
+      return newState;
+    },
+    [saveState]
+  );
+
+  return useReducer(reducerLocalStorage, savedState);
+};
+
 export function ThemeProvider(props: any) {
-  const [state, dispatch] = useReducer(themeReducer, initialState);
+  const [state, dispatch] = usePersistReducer();
 
   return <ThemeContext.Provider value={{ state, dispatch }}>{props!.children}</ThemeContext.Provider>;
 }
