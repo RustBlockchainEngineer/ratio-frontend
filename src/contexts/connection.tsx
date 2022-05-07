4e364f0e4c493a81f9368d78114051ce085b9127
zhao <zhaohuili0210@gmail.com>
2022-05-08 00:25:00 +0200

    fix: rpc endpoint

--- src/contexts/connection.tsx
+++ src/contexts/connection.tsx
@@ -24,9 +24,7 @@ export type ENV = 'mainnet-beta' | 'testnet' | 'devnet' | 'localnet';
 export const ENDPOINTS = [
   {
     name: 'mainnet-beta' as ENV,
-    // endpoint: 'https://solana-api.projectserum.com/',
-    // endpoint: 'https://ratio.genesysgo.net/',
-    endpoint: 'https://cropper.rpcpool.com/',
+    endpoint: 'https://ratio.genesysgo.net/',
     chainID: ChainID.MainnetBeta,
   },
   {
