5db0e6685929ed931950a9844487a248c0d441e6
WonderDev21 <wonder.dev21@gmail.com>
2022-05-09 02:04:34 +0900

    fix: fixed refreshing delay issue

--- src/contexts/connection.tsx
+++ src/contexts/connection.tsx
@@ -15,7 +15,7 @@ import { useLocalStorageState } from '../utils/utils';
 import { notify } from '../utils/notifications';
 import { setProgramIds } from '../utils/ids';
 import { WalletAdapter } from './wallet';
-// import { cache, getMultipleAccounts, MintParser } from './accounts';
+import { cache } from './accounts';
 import { API_ENDPOINT } from '../constants';
 
 export type ENV = 'mainnet-beta' | 'testnet' | 'devnet' | 'localnet';
@@ -56,8 +56,6 @@ interface ConnectionConfig {
   setSlippage: (val: number) => void;
   env: ENV;
   setEndpoint: (val: string) => void;
-  // tokens: TokenInfo[];
-  // tokenMap: Map<string, TokenInfo>;
 }
 
 const ConnectionContext = React.createContext<ConnectionConfig>({
@@ -70,8 +68,6 @@ const ConnectionContext = React.createContext<ConnectionConfig>({
   connection: new Connection(DEFAULT.endpoint, 'recent'),
   sendConnection: new Connection(DEFAULT.endpoint, 'recent'),
   env: DEFAULT.name,
-  // tokens: [],
-  // tokenMap: new Map<string, TokenInfo>(),
 });
 
 const getRpcAuthToken = async () => {
@@ -105,33 +101,9 @@ export function ConnectionProvider({ children = undefined as any }) {
   const chain = ENDPOINTS.find((end) => end.endpoint === endpoint) || DEFAULT;
   const env = chain.name;
 
-  // const [tokens, setTokens] = useState<TokenInfo[]>([]);
-  // const [tokenMap, setTokenMap] = useState<Map<string, TokenInfo>>(new Map());
-  // useEffect(() => {
-  //   cache.clear();
-  //   // fetch token files
-  //   (async () => {
-  //     const res = await new TokenListProvider().resolve();
-  //     const list = res.filterByChainId(chain.chainID).excludeByTag('nft').getList();
-  //     const knownMints = list.reduce((map, item) => {
-  //       map.set(item.address, item);
-  //       return map;
-  //     }, new Map<string, TokenInfo>());
-
-  //     const accounts = await getMultipleAccounts(connection, [...knownMints.keys()], 'single');
-  //     accounts.keys.forEach((key, index) => {
-  //       const account = accounts.array[index];
-  //       if (!account) {
-  //         return;
-  //       }
-
-  //       cache.add(new PublicKey(key), account, MintParser);
-  //     });
-
-  //     setTokenMap(knownMints);
-  //     setTokens(list);
-  //   })();
-  // }, [connection, chain]);
+  useEffect(() => {
+    cache.clear();
+  }, [connection, chain]);
 
   setProgramIds(env);
 
